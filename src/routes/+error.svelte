c42e73b12ac9e3aa002bd0b5771535b0be8fa95f
Jeremiah Ikwuje <ikwuje24@gmail.com>
2023-05-08 22:11:42 +0100

    fix: change 404 text

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,14 +5,14 @@
 <div class="flex items-center justify-center h-screen p-8">
     <div class="bg-white w-[70%] mx-auto py-24 px-4">
         <div class="flex flex-col items-center">
-        <h1 class="font-bold text-3xl text-primary lg:text-6xl">404</h1>
+        <h1 class="font-bold text-3xl text-primary lg:text-6xl text-center">Something went wrong</h1>
 
         <h6 class="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
             <span class="text-red-500">Oops!</span> Page {$page.error.message}
         </h6>
 
         <p class="mb-8 text-center text-gray-500 md:text-lg">
-            The page you’re looking for doesn’t exist.
+            Either the page you’re looking for doesn’t exist or an error occured.
         </p>
 
         <a
