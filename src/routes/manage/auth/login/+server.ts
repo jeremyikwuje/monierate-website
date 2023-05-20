e86c8845a15f2feba0901786c6a246dc091ad53f
Jeremiah Ikwuje <ikwuje24@gmail.com>
2023-05-20 01:38:58 +0100

    feat: add manage routes and update api url

---
+++ src/routes/manage/auth/login/+server.ts
@@ -0,0 +1,29 @@
+import { basicHeader, getEndpoint  } from "$lib/helper.svelte";
+import { json } from "@sveltejs/kit";
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request, cookies })
+{
+    const { id, password } = await request.json();
+    const payload = {
+        password,
+        id,
+    };
+    const endpoint = getEndpoint("/public/auth/login");
+    const res = await fetch(endpoint, basicHeader('POST', payload));
+
+    const result = await res.json();
+    console.log(result);
+
+    if (result.status == "success")
+    {
+        await cookies.delete('access_token');
+        
+        cookies.set("access_token", result.data.token, {
+            path: "/manage",
+            maxAge: 60 * 30,
+        })
+    }
+
+    return json(result);
+}
