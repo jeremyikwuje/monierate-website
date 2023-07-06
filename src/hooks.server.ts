6633358c6940ccefe468e34ad3d0703044204be3
Jeremy Ikwuje <ikwuje24@gmail.com>
2023-07-06 11:25:47 +0100

    feat: Add bybit

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,16 +2,6 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({event, resolve}) => {
-    const sessionid = event.cookies.get('access_token');
-
-    if (!sessionid) {
-        return await resolve(event);
-    }
-
-    event.locals.access = {
-        token: sessionid
-    };
-
     return await resolve(event);
 }
 
