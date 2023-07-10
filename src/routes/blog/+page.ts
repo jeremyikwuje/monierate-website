73067f5f7864760041aab307d240217fb618e78e
Jeremy Ikwuje <ikwuje24@gmail.com>
2023-07-10 05:57:40 +0100

    feat: clear blog/page.ts

--- src/routes/blog/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { Post } from '$lib/blog/types'
-
-export async function load({ fetch }) {
-	const response = await fetch(`/api/blog`)
-	const posts: Post[] = await response.json()
-	return { posts }
-}
