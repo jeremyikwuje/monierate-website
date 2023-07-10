39d420a9b9c166b5b4e6bbbaa6a196ebc228befe
Jeremy Ikwuje <ikwuje24@gmail.com>
2023-07-10 05:36:46 +0100

    feat: increase load time on blog content page

--- src/routes/blog/[tag]/[slug]/+page.svelte
+++ src/routes/blog/[tag]/[slug]/+page.svelte
@@ -3,39 +3,63 @@
 	import { formatDate } from '$lib/blog/utils'
 
 	export let data
+
+	async function getPost() {
+		const post = await import(`../../../../blog/${data.slug}.md`)
+
+		console.log(post.default)
+		console.log(post.metadata)
+
+		return post
+	}
+
+	let post = getPost()
 </script>
 
 <!-- SEO -->
 <svelte:head>
-	<title>{data.meta.title} &mdash; Monierate</title>
-	<meta name="description" content={data.meta.description} />
+	{#await getPost()}
+		<title></title>
+    {:then value}
+	<title>{value.metadata.title} &mdash; Monierate</title>
+	<meta name="description" content={value.metadata.description} />
 	<meta property="og:type" content="article" />
-	<meta property="og:title" content={data.meta.title} />
-	<meta property="og:description" content={data.meta.description} />
-	<meta property="og:image" content={data.meta.image} />
-	<meta property="og:url" content={config.url}/{data.meta.tag}/{data.slug} />
+	<meta property="og:title" content={value.metadata.title} />
+	<meta property="og:description" content={value.metadata.description} />
+	<meta property="og:image" content={value.metadata.image} />
+	<meta property="og:url" content={config.url}/{value.metadata.tag}/{value.metadata.slug} />
+	{:catch error}
+		{error.message}
+	{/await}
 </svelte:head>
 
 
 <main class="container mt-8 mb-8 pb-8 md:mt-16 md:mb-8">
+
+	{#await getPost()}
+        <span class="loading">Loading...</span>
+    {:then value}
 	<header class="mb-4 md:mb-6 not-format text-center">
 		<!-- Tags -->
 		<div class="tags text-center">
-			<a href="/blog/{data.meta.tag}" class="inline-block text-green-500 font-semibold text-xs mb-1 leading-none mr-2 last:mr-0">&num;{data.meta.tag.toUpperCase()}</a>
+			<a href="/blog/{value.metadata.tag}" class="inline-block text-green-500 font-semibold text-xs mb-1 leading-none mr-2 last:mr-0">&num;{value.metadata.tag.toUpperCase()}</a>
 		</div>
 		<!-- Title -->
-		<h1 class="mb-2 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-2 lg:text-4xl dark:text-gray-300 dark:font-semibold">{data.meta.title}</h1>
-		<p>Published {formatDate(data.meta.createdAt)}</p>
+		<h1 class="mb-2 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-2 lg:text-4xl dark:text-gray-300 dark:font-semibold">{value.metadata.title}</h1>
+		<p>Published {formatDate(value.metadata.createdAt)}</p>
 	</header>
 
 	<div class="bg-white dark:bg-black flex justify-between px-4 mx-auto max-w-screen-xl">
 		<article id="entry" class="mx-auto w-full max-w-2xl format text-gray-800 dark:text-gray-300 format-sm sm:format-base lg:format-lg format-primary dark:format-invert lg:w-[700px]">
 			<!-- Post -->
 			<div class="post-content">
-				<svelte:component this={data.content} />
+				<svelte:component this={value.default} />
 			</div>
 		</article>
 	</div>
+    {:catch error}
+        {error.message}
+    {/await}
 </main>
 
 <style>
--- src/routes/blog/[tag]/[slug]/+page.ts
+++ src/routes/blog/[tag]/[slug]/+page.ts
@@ -2,11 +2,7 @@ import { error } from '@sveltejs/kit'
 
 export async function load({ params }) {
 	try {
-		const post = await import(`../../../../blog/${params.slug}.md`)
-
 		return {
-			content: post.default,
-			meta: post.metadata,
 			slug: params.slug
 		}
 	} catch (e) {
