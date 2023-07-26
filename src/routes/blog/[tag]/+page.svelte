47547a581801812c86a68695935acc2147cf7579
Jeremy Ikwuje <ikwuje24@gmail.com>
2023-07-26 10:58:10 +0100

    feat: load blog tag posts from server not client

--- src/routes/blog/[tag]/+page.svelte
+++ src/routes/blog/[tag]/+page.svelte
@@ -3,12 +3,7 @@
 	import type { Post } from '$lib/blog/types'
 
 	export let data
-
-	async function getPosts() {
-		const response = await fetch(`/api/blog?tag=${data.tag}`)
-		const posts: Post[] = await response.json()
-		return posts
-	}
+	let posts = data.posts
 </script>
 
 <svelte:head>
@@ -28,12 +23,9 @@
 
 	<!-- Posts -->
 	<div class="container bg-white px-4 shadow-md dark:bg-gray-900">
-		<section id="entries" class="mt-8 min-h-[100vh]">
-			{#await getPosts()}
-				<span class="loading">Loading...</span>
-			{:then values}
+		<section id="entries" class="min-h-[100vh]">
 			<ul class="posts">
-				{#each values as post}
+				{#each posts as post}
 					<li class="flex flex-col md:flex-row justify-between item-left md:items-center mb-4 py-6 border-b-2 border-dotted border-gray-300 dark:border-gray-700 last:border-none">
 						<span class="block">
 							<a href="/blog/{post.tag}" class="inline-block text-gray-600 dark:text-green-500 font-semibold text-xs mb-1 leading-none mb-4">{post.tag.toUpperCase()}</a>
@@ -44,9 +36,6 @@
 					</li>
 				{/each}
 			</ul>
-			{:catch error}
-				{error.message}
-			{/await}
 		</section>
 	</div>
 </div>
--- src/routes/blog/[tag]/+page.ts
+++ src/routes/blog/[tag]/+page.ts
@@ -1,5 +1,18 @@
 
-export async function load({ fetch, params }) {
-	const tag = params.tag || ''
-	return { tag }
+import type { Post } from '$lib/blog/types'
+
+export async function load({ params }) {
+	const tag = params.tag
+    const posts = await getPosts(tag || '')
+
+    return {
+        posts: posts,
+		tag: tag
+    }
+}
+
+async function getPosts(tag: string) {
+    const response = await fetch(`/api/blog?tag=${tag}`)
+    const posts: Post[] = await response.json()
+    return posts
 }
