1c419128fb04d8f245c76f8636535c379becf329
Jeremy Ikwuje <ikwuje24@gmail.com>
2023-11-08 22:30:57 +0100

    feat: add noones

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -258,6 +258,17 @@ function hidePromotionBar() {
 </div>
 {/if}
 
+<!-- Landscape Top/Bottom -->
+<!-- <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9886145381404391"
+     data-ad-slot="3727635247"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script> -->
+
 <style>
     .nav-link {
         @apply flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0;
