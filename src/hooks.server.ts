2224e3054db458917ee18f0915493b16218d9629
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-01-27 20:53:02 +0100

    feat: add security headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,35 @@
 
 import type { Handle } from '@sveltejs/kit';
 
+const securityHeaders = {
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+    'Cross-Origin-Opener-Policy': 'same-origin',
+    'X-XSS-Protection': '0',
+    "X-Xss-Protection" : "1; mode=block",
+	"X-Frame-Options" : "DENY",
+	"X-Content-Type-Options" : "nosniff",
+    "Content-Security-Policy" : "upgrade-insecure-requests",
+	"Strict-Transport-Security" : "max-age=1000",
+	"Referrer-Policy" : "strict-origin-when-cross-origin",
+}
+
+const removeHeaders = [
+    "Public-Key-Pins",
+    "X-Powered-By",
+    "X-AspNet-Version",
+]
+
 export const handle: Handle = async ({event, resolve}) => {
-    return await resolve(event);
+    const response = await resolve(event);
+    Object.entries(securityHeaders).forEach(
+        ([header, value]) => response.headers.set(header, value)
+    );
+
+    // remove headers we don't want exposed
+    removeHeaders.forEach( (name) => {
+		response.headers.delete(name)
+	})
+
+    return response;
 }
 
