00439f90864d0e8ff9a4a7d9c13d44fa6b81280d
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-01-27 20:56:58 +0100

    feat: update security headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,12 +4,12 @@ import type { Handle } from '@sveltejs/kit';
 const securityHeaders = {
     'Cross-Origin-Embedder-Policy': 'require-corp',
     'Cross-Origin-Opener-Policy': 'same-origin',
-    'X-XSS-Protection': '0',
+    'X-XSS-Protection': '1; mode=block',
     "X-Xss-Protection" : "1; mode=block",
-	"X-Frame-Options" : "DENY",
+    'X-Frame-Options': 'SAMEORIGIN',
 	"X-Content-Type-Options" : "nosniff",
     "Content-Security-Policy" : "upgrade-insecure-requests",
-	"Strict-Transport-Security" : "max-age=1000",
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
 	"Referrer-Policy" : "strict-origin-when-cross-origin",
 }
 
