dad7f6bb391daead784480e0eb72041c76dee7a8
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-02-20 04:34:14 +0100

    fix: btoa not defined

--- src/routes/api/alerting/+server.ts
+++ src/routes/api/alerting/+server.ts
@@ -1,5 +1,5 @@
 import { bearer } from "$lib/functions.js";
-import { basicAuth, getEndpoint  } from "$lib/helper.svelte";
+import { getEndpoint  } from "$lib/helper.svelte";
 import { json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
@@ -48,4 +48,46 @@ export async function POST({ request, fetch })
     console.log(result);
 
     return json(result);
+}
+
+function universalBtoa(str: any) {
+    try {
+      return btoa(str);
+    } catch (err) {
+      return Buffer.from(str, 'binary').toString('base64');
+    }
+};
+  
+function universalAtob(b64Encoded: any): any {
+    try {
+        return atob(b64Encoded);
+    } catch (err) {
+        return Buffer.from(b64Encoded, 'base64').toString('binary');
+    }
+};
+
+
+function basicAuth(method: string, body: any, level = "api")
+{
+    const apiAuth = `changemoney_api:N4&**S%Vl0C7MubL`
+    const systemAuth = `ikwuje:xaS@Di2t#7Qry19M`
+
+    let basicToken = universalBtoa(apiAuth);
+
+    if (level == 'system') {
+        basicToken = universalBtoa(systemAuth);
+    }
+
+    const request: any = {
+        headers: {
+            "Authorization": `Basic ${basicToken}`,
+            "Content-Type": "application/json",
+        },
+        method: method,
+    }
+
+    if (method != 'GET')
+        request.body = JSON.stringify(body);
+
+    return request;
 }
