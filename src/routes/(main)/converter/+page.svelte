7950b95b14bb23c029cb7321364b2c1a645eeacf
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-03-02 08:11:50 +0100

    feat: add sponsor button

--- src/routes/(main)/converter/+page.svelte
+++ src/routes/(main)/converter/+page.svelte
@@ -238,11 +238,17 @@ convertNow()
                 </div>
                 
                 <span class="block mt-12 mb-4">
-                    <a href="/alerts" class="button">
+                    <a href="/alerts" class="block button w-full md:inline-block md:w-auto mr-4 mb-4 ">
                         Get alert
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-4 h-4 ml-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
                         </svg>                          
+                    </a>
+                    <a href="https://tinyurl.com/beansAppbuybutton" class="block button bg-primary font-bold w-full md:inline-block md:w-auto">
+                        Get The Best Rate on Beans App
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-4 h-4 ml-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                        </svg> 
                     </a>
                 </span>
             </div>
