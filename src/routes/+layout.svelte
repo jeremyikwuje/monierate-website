7ff3dfc97b024adc045a3aa29b735989be423e89
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-07-11 14:09:42 +0100

    feat: adjust top banner text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -107,7 +107,7 @@ function hidePromotionBar() {
             </span>
             <span>
                 Send Pounds & Euros to 55+ Countries, Including UK
-                <a href="https://tinyurl.com/cambridge-currences-top-banner" class="ml-2 break-word text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline text-wrap">
+                <a href="https://tinyurl.com/cambridge-currences-top-banner" class="break-word text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline text-wrap">
                     Cambridge Currencies
                     <svg class="inline-block w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
