ff167daf8d2be54f033190ffb1f964a5792ef761
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-07-11 14:08:36 +0100

    feat: adjust top banner text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -96,19 +96,20 @@ function hidePromotionBar() {
 <svelte:head>
 </svelte:head>
 
-<div id="bottom-banner" tabindex="-1" class="fixed top-0 mb-8 start-0 z-50 flex justify-between w-full p-4 border-t border-gray-200 bg-gray-100 dark:bg-gray-700 dark:border-gray-600">
+<div id="bottom-banner" tabindex="-1" class="fixed top-0 mb-8 start-0 z-50 flex justify-between w-full p-4 border-t border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
     <div class="flex items-center mx-auto">
-        <p class="flex items-center text-sm font-normal text-gray-600 dark:text-gray-400">
+        <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
             <span class="inline-flex p-1 me-3 bg-gray-200 rounded-full dark:bg-gray-600 w-6 h-6 items-center justify-center">
                 <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M18.435 7.546A2.32 2.32 0 0 1 17.7 5.77a3.354 3.354 0 0 0-3.47-3.47 2.322 2.322 0 0 1-1.776-.736 3.357 3.357 0 0 0-4.907 0 2.281 2.281 0 0 1-1.776.736 3.414 3.414 0 0 0-2.489.981 3.372 3.372 0 0 0-.982 2.49 2.319 2.319 0 0 1-.736 1.775 3.36 3.36 0 0 0 0 4.908A2.317 2.317 0 0 1 2.3 14.23a3.356 3.356 0 0 0 3.47 3.47 2.318 2.318 0 0 1 1.777.737 3.36 3.36 0 0 0 4.907 0 2.36 2.36 0 0 1 1.776-.737 3.356 3.356 0 0 0 3.469-3.47 2.319 2.319 0 0 1 .736-1.775 3.359 3.359 0 0 0 0-4.908ZM8.5 5.5a1 1 0 1 1 0 2 1 1 0 0 1 0-2Zm3 9.063a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm2.207-6.856-6 6a1 1 0 0 1-1.414-1.414l6-6a1 1 0 0 1 1.414 1.414Z"/>
                 </svg>
                 <span class="sr-only">Discount</span>
             </span>
-            <span>Send Pounds & Euros to 55+ Countries, Including UK
-                <a href="https://tinyurl.com/cambridge-currences-top-banner" class="flex items-center ms-0 text-sm font-medium text-blue-600 md:ms-1 md:inline-flex dark:text-blue-500 hover:underline">
+            <span>
+                Send Pounds & Euros to 55+ Countries, Including UK
+                <a href="https://tinyurl.com/cambridge-currences-top-banner" class="ml-2 break-word text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline text-wrap">
                     Cambridge Currencies
-                    <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <svg class="inline-block w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
                 </a>
