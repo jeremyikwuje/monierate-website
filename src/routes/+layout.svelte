0145e7df642cfa64e4befddccdf26b90886135fd
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-08-06 10:39:45 +0100

    feat: change cedar banner on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -208,7 +208,7 @@ function hidePromotionBar() {
         <div class="cedar-money">
             <a href="https://tinyurl.com/4zuasuhv" target="_blank">
                 <img 
-                    src="https://monierate.com/cedar-banner-1.gif"
+                    src="https://monierate.com/uploads/cedar-banner-pay-in-NGN-XOF-GHS-KES.gif"
                     alt="Cedar Money"
                     class="mx-auto max-w-full md:w-[800px] md:h-[114px]">
             </a>
