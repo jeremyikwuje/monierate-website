fa0ef907ad2956e4c070c38b429b6e9318a1b429
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-08-06 10:43:07 +0100

    feat: change cedar banner on layout

--- src/routes/(main)/converter/+page.svelte
+++ src/routes/(main)/converter/+page.svelte
@@ -170,7 +170,7 @@ convertNow()
         <div class="cedar-money">
             <a href="https://tinyurl.com/4zuasuhv" target="_blank">
                 <img 
-                    src="https://monierate.com/cedar-banner-1.gif"
+                    src="https://monierate.com/uploads/cedar-banner-pay-in-NGN-XOF-GHS-KES.gif"
                     alt="Cedar Money"
                     class="mx-auto max-w-full md:w-[800px] md:h-[114px]">
             </a>
--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -4,7 +4,7 @@
         <div class="cedar-money">
             <a href="https://tinyurl.com/4zuasuhv" target="_blank">
                 <img 
-                    src="https://monierate.com/cedar-banner-1.gif"
+                    src="https://monierate.com/uploads/cedar-banner-pay-in-NGN-XOF-GHS-KES.gif"
                     alt="Cedar Money"
                     class="mx-auto max-w-full md:w-[800px] md:h-[114px]">
             </a>
