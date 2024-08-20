2cd2dba0abc8882c95fa71030086169ceb34be6f
Jeremy Ikwuje <ikwuje24@gmail.com>
2024-08-20 14:03:16 +0100

    feat: add new cedar banner to blog page

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -2,9 +2,9 @@
     <div class="container">
         <!-- Landscape Top/Bottom -->
         <div class="cedar-money">
-            <a href="https://tinyurl.com/4zuasuhv" target="_blank">
+            <a href="https://tinyurl.com/cedar-app-banner-1" target="_blank">
                 <img 
-                    src="https://monierate.com/uploads/cedar-banner-pay-in-NGN-XOF-GHS-KES.gif"
+                    src="https://monierate.com/uploads/cedar-1-compressed.gif"
                     alt="Cedar Money"
                     class="mx-auto max-w-full md:w-[800px] md:h-[114px]">
             </a>
