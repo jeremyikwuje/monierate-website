1bcab633e2bd8b2e02ce70e7fddb3950500c56a9
dev-henen <henen.dev@gmail.com>
2024-10-14 08:57:45 +0100

    Removed the city and more columns on the swift code banks table

--- src/routes/(main)/[country]/bank-codes/swift-code/+page.svelte
+++ src/routes/(main)/[country]/bank-codes/swift-code/+page.svelte
@@ -40,21 +40,15 @@
 							<th scope="col" class="pl-2 py-3 w-[50%] md:w-[25%] md:pl-4 font-bold font-bitter">
 								Bank
 							</th>
-							<th scope="col" class="py-3 font-bold font-bitter text-center pr-2 md:pr-4">
-								City
-							</th>
 							<th scope="col" class="py-3 font-bold font-bitter text-right pr-2 md:pr-4">
 								SWIFT Code
-							</th>
-							<th scope="col" class="py-3 font-bold font-bitter text-right pr-2 md:pr-4 md:w-[20%]">
-								More
 							</th>
 						</tr>
 					</thead>
 					<tbody class="banks">
 						{#each Object.values(banksData) as data}
 							<tr class="border-t border-gray-100 dark:border-gray-700">
-								<td class="pl-2 md:pl-4 w-[50%] md:w-[25%]">
+								<td class="pl-2 md:pl-4 w-[50%] md:w-[25%] py-3">
 									<a
 										href="/{countryCode}/bank-codes/swift-code/{data.id}"
 										class="flex items-center"
@@ -72,18 +66,14 @@
 										<span class="bank-title ml-2">{data.name}</span>
 									</a>
 								</td>
-								<td class="text-center pr-4 md:pr-6 w-[20%] break-words">
-									{data.city ? data.city : ''}
-								</td>
-								<td class="text-right pr-4 md:pr-6 w-[20%] break-words">
-									{data.swift.codes ? data.swift.codes.join(', ') : ''}
-								</td>
-								<td class="text-right py-2 pr-2 md:pr-4 w-[100px]">
+								<td class="text-right pr-4 md:pr-6 w-[20%] break-words py-3">
 									<a
 										href="/{countryCode}/bank-codes/swift-code/{data.id}"
-										class="inline-block p-2 border border-gray-300 text-black rounded-lg text-xs font-semibold mb-2 dark:text-light whitespace-nowrap"
-										>more</a
+										class="text-decoration underline bank-code"
+										title=""
 									>
+									    {data.swift.codes ? data.swift.codes.join(', ') : ''}
+								    </a>
 								</td>
 							</tr>
 						{/each}
@@ -104,5 +94,8 @@
 	}
 	.bank-title {
 		@apply font-semibold md:text-lg capitalize text-gray-800 dark:text-gray-300 whitespace-nowrap;
+	}
+	.bank-code {
+		@apply font-normal text-gray-800 dark:text-gray-300 whitespace-nowrap;
 	}
 </style>
