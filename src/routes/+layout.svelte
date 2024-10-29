11fc3c6b6a6aef704b783648cd2648fba2934aa4
dev-henen <henen.dev@gmail.com>
2024-10-29 08:42:33 +0100

    changed the USSD Codes link in the footer navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -268,7 +268,7 @@ function hidePromotionBar() {
                 <li><a href="/alerts">Price Alerts</a></li>
                 <li><a href="/converter">Currency Converter</a></li>
                 <li><a href="/ng/compare">Compare exchange rates</a></li>
-                <li><a href="/bank-codes">Bank USSD Codes</a></li>
+                <li><a href="/ng/ussd-codes">Bank USSD Codes</a></li>
                 <li><a href="/ng/swift-codes">SWIFT Codes</a></li>
             </ul>
         </div>
