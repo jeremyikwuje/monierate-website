17e7c4e586a7670884339c0ea8ea50289de12960
dev-henen <henen.dev@gmail.com>
2024-11-05 17:47:08 +0100

    Oganized PNG and SVG image formats v3

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -136,7 +136,7 @@
                                 <a href="/converter/{rate.changer_code}?Amount=1&From=USD&To=NGN" class="flex items-center" title="{providers[rate.changer_code].name} dollar to naira rate.">
                                     <span class="changer-icon rounded-full overflow-hidden">
                                         <picture>
-                                            <source srcset="/icons/svg/{rate.changer_code}.svg" type="image/svg+xml">       
+                                            <source srcset="/icons/svg/{rate.changer_code}.svg" type="image/svg+xml">    
                                             <source srcset="/icons/png/{rate.changer_code}.png" type="image/png">
                                             <img width="22px" height="22px" src="/icons/svg/{rate.changer_code}.svg" class="" alt="{providers[rate.changer_code].name} icon">
                                         </picture>
