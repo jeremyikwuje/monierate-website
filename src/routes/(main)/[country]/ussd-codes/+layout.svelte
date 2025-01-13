71210ca3091c9cff404c1d56c3809613915a3f11
dev-henen <henen.dev@gmail.com>
2025-01-13 13:32:01 +0100

    changed all advert banner

--- src/routes/(main)/[country]/ussd-codes/+layout.svelte
+++ src/routes/(main)/[country]/ussd-codes/+layout.svelte
@@ -21,11 +21,11 @@
 			<a href="https://bit.ly/3zfhZgz" target="_blank">
 				<picture>
 
-                    <source srcset="/media/banners/cedar-converter-2.avif" type="image/avif">    
-                    <source srcset="/media/banners/cedar-converter-2.gif" type="image/gif">
+                    <source srcset="/media/banners/cedar-converter-2.avif?v=1" type="image/avif">    
+                    <source srcset="/media/banners/cedar-converter-2.gif?v=1" type="image/gif">
 
 					<img
-						src="/media/banners/cedar-converter-2.avif"
+						src="/media/banners/cedar-converter-2.avif?v=1"
 						alt="Cedar Money"
 						width="600"
 						height="114px"
--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -6,11 +6,11 @@
                 
                 <picture>
 
-                    <source srcset="/media/banners/cedar-blog-2.avif" type="image/avif">    
-                    <source srcset="/media/banners/cedar-blog-2.gif" type="image/gif">
+                    <source srcset="/media/banners/cedar-blog-2.avif?v=1" type="image/avif">    
+                    <source srcset="/media/banners/cedar-blog-2.gif?v=1" type="image/gif">
 
                     <img 
-                        src="/media/banners/cedar-blog-2.avif"
+                        src="/media/banners/cedar-blog-2.avif?v=1"
                         alt="Cedar Money"
                         width="800px"
                         height="114px"
