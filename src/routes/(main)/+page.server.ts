e2ea401a1be1056269aed2a56ef8bcb708f5b27f
dev-henen <henen.dev@gmail.com>
2025-01-25 09:44:13 +0100

    added a new rate column in the highlights section and improved highlights rendering

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -1,10 +1,14 @@
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from './$types'
 import { get_changers } from "$lib/server/changer.service";
+import { get_pairs_changers } from "$lib/server/pair.service";
 
 export const load: PageServerLoad = async ({ fetch }) => {
     try {
         let providers = await get_changers();
+        let remittance = await get_pairs_changers(`usdngn`, 'remittance');
+        let ramp = await get_pairs_changers(`usdngn`, 'ramp');
+        let card = await get_pairs_changers(`usdngn`, 'card');
 
         if (providers.length == 0) {
             throw error(500, {
@@ -21,7 +25,10 @@ export const load: PageServerLoad = async ({ fetch }) => {
         providers = tmp_providers;
 
         return {
-            providers
+            providers,
+            remittance,
+            ramp,
+            card
         }
     }
     catch(error: any) {
--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -29,10 +29,13 @@
 	let rates = pair.changers;
 	const providers: Record<string, Changer> = data.providers || {};
 	const total = Object.entries(rates).length;
+	const lastNRows = (arr:any, n:number) => arr.slice(-n);
+	const lastNRowsRates = lastNRows(rates, 5);
 	let groupRates: Record<string, PairChanger[]> = {
-		remittance: [],
-		ramp: [],
-		card: []
+		remittance: data.remittance,
+		ramp: data.ramp,
+		card: data.card,
+		newRates: lastNRowsRates,
 	};
 
 	// sort rates in decending order by price_buy;
@@ -51,10 +54,7 @@
 		return providerName.toLowerCase().includes(searchTerm.toLowerCase());
 	});
 
-	let isLoading = true;
-	let hasResults = false;
-	let resultsLoaded = false;
-
+	let newResult: ChangerRate[] = [];
 	let sendingResult: ChangerRate[] = [];
 	let buyingResult: ChangerRate[] = [];
 	let sellingResult: ChangerRate[] = [];
@@ -69,26 +69,6 @@
 
 		updateMobileStatus();
 		window.addEventListener('resize', updateMobileStatus);
-	}
-
-	async function getPairChangers(
-		pair_code: string,
-		changer_service: string
-	): Promise<PairChanger[]> {
-		try {
-			const response = await fetch(
-				`/api/pairs/changers?pair_code=${pair_code}&changer_service=${changer_service}`
-			);
-
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`);
-			}
-
-			return await response.json();
-		} catch (error) {
-			console.error('Error fetching pair changers:', error);
-			return [];
-		}
 	}
 
 	function findSupportedPlatforms(
@@ -124,62 +104,45 @@
 		}
 	}
 
-	$: {
-		if (groupRates.remittance && groupRates.ramp && groupRates.card && total > 0) {
-			isLoading = true;
-			hasResults = false;
+	if (total > 0) {
 
-			try {
-				if (groupRates.remittance.length > 0) {
-					sendingResult = findSupportedPlatforms(
-						providers,
-						groupRates.remittance,
-						true,
-						false
-					).slice(0, 5);
-				}
-				if (groupRates.ramp.length > 0) {
-					buyingResult = findSupportedPlatforms(providers, groupRates.ramp, false, true).slice(
-						0,
-						5
-					);
-					sellingResult = findSupportedPlatforms(providers, groupRates.ramp, true, false).slice(
-						0,
-						5
-					);
-				}
-				if (groupRates.card.length > 0) {
-					fundingResult = findSupportedPlatforms(providers, groupRates.card, false, true).slice(
-						0,
-						5
-					);
-				}
-
-				hasResults = sendingResult.length > 0;
-				resultsLoaded = true;
-			} catch (error) {
-				console.error('Results processing error:', error);
-			} finally {
-				setTimeout(() => {
-					isLoading = false;
-				}, 300);
+		try {
+			if (groupRates.newRates.length > 0) {
+				newResult = findSupportedPlatforms(
+					providers,
+					groupRates.newRates,
+					true,
+					false
+				).slice(0, 5);
 			}
-		}
-	}
+			
+			if (groupRates.remittance && groupRates.remittance.length > 0) {
+				sendingResult = findSupportedPlatforms(
+					providers,
+					groupRates.remittance,
+					true,
+					false
+				).slice(0, 5);
+			}
+			if (groupRates.ramp && groupRates.ramp.length > 0) {
+				buyingResult = findSupportedPlatforms(providers, groupRates.ramp, false, true).slice(
+					0,
+					5
+				);
+				sellingResult = findSupportedPlatforms(providers, groupRates.ramp, true, false).slice(
+					0,
+					5
+				);
+			}
+			if (groupRates.card && groupRates.card.length > 0) {
+				fundingResult = findSupportedPlatforms(providers, groupRates.card, false, true).slice(
+					0,
+					5
+				);
+			}
 
-	async function updatePairChangers() {
-		groupRates.remittance = await getPairChangers(`usdngn`, 'remittance');
-
-		// Fetch buying and selling (ramp) rates
-		groupRates.ramp = await getPairChangers(`usdngn`, 'ramp');
-
-		// Fetch funding (card) rates
-		groupRates.card = await getPairChangers(`usdngn`, 'card');
-	}
-
-	$: {
-		if (typeof window !== 'undefined') {
-			updatePairChangers();
+		} catch (error) {
+			console.error('Results processing error:', error);
 		}
 	}
 
@@ -276,6 +239,55 @@
 
 	{#if showHighlights}
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+			<!--New-->
+			{#if newResult.length > 0}
+				<div
+					class="bg-white dark:bg-gray-900 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700 p-4"
+				>
+					<div class="flex justify-between items-center mb-4">
+						<span class="block text-[0.8em] md:text-[1em] font-semibold text-gray-800 dark:text-white">
+							🔥 New Rate
+						</span>
+					</div>
+					{#each newResult as { rate, changer }}
+						<ul>
+							<li class="flex justify-between text-sm text-gray-800 dark:text-gray-200 mb-2">
+								<a
+									href="/converter/{rate.changer_code}?Amount=1&From=usd&To=ngn"
+									class="flex items-center"
+									title="{changer.name} dollar to naira rate."
+								>
+									<span class="changer-icon-sm">
+										<picture>
+											<source srcset="/icons/svg/{rate.changer_code}.svg" type="image/svg+xml" />
+											<source srcset="/icons/png/{rate.changer_code}.png" type="image/png" />
+											<img
+												width="16px"
+												height="16px"
+												src="/icons/svg/{rate.changer_code}.svg"
+												class="rounded-full"
+												alt="{changer.name} icon"
+											/>
+										</picture>
+									</span>
+									<span class="text-gray-500 dark:text-gray-300 text-sm">{changer.name}</span>
+								</a>
+								<div>
+									<span class="text-sm">
+										{#if Math.round(rate.price_buy) === 0}
+											-
+										{:else}
+											₦ {Math.round(rate.price_buy)}
+										{/if}
+									</span>
+									<small class="changer-rate-base">per $1</small>
+								</div>
+							</li>
+						</ul>
+					{/each}
+				</div>
+			{/if}
+			
 			<!--BUYING-->
 			{#if buyingResult.length > 0}
 				<div
@@ -430,7 +442,7 @@
 			<!--FUNDING-->
 			{#if fundingResult.length > 0}
 				<div
-					class="bg-white dark:bg-gray-900 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700 p-4"
+					class="bg-white dark:bg-gray-900 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700 p-4 md:hidden"
 				>
 					<div class="flex justify-between items-center mb-4">
 						<span class="block text-[0.8em] md:text-[1em] font-semibold text-gray-800 dark:text-white">
