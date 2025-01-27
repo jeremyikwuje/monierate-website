2b05b72b9dc1d4f4393e4106c970af772042619a
dev-henen <henen.dev@gmail.com>
2025-01-27 17:56:33 +0100

    Changed nala to redima in the highlights new section

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -29,7 +29,7 @@
 	let rates = pair.changers;
 	const providers: Record<string, Changer> = data.providers || {};
 	const total = Object.entries(rates).length;
-	let newestProviders = ['koyn', 'cleva', 'coinex', 'nala', 'vertofx'];
+	let newestProviders = ['ridima', 'koyn', 'cleva', 'coinex', 'vertofx'];
 	let getNewestProviders: any = {};
 	newestProviders.map((code: string) => {
 		getNewestProviders[code] = providers[code];
