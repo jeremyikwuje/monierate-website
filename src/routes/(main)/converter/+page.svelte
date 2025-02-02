c5ba8266e2486585e949f9ccb2325e95c8b2508b
Jeremy Ikwuje <ikwuje24@gmail.com>
2025-02-02 05:29:23 +0200

    feat: add koyn native button

--- src/routes/(main)/converter/+page.svelte
+++ src/routes/(main)/converter/+page.svelte
@@ -537,7 +537,7 @@
 				</div>
 
 				<span class="block mt-12 mb-4">
-					<a href="https://tinyurl.com/koyn-native-button" class="block button buy font-bold w-full md:inline-block md:w-auto mr-4">
+					<a href="https://tinyurl.com/koyn-native-button-1" class="block button buy font-bold w-full md:inline-block md:w-auto mr-4">
 						Sell Crypto on Koyn
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-4 h-4 ml-2">
 								<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
