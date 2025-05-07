28241e7f162f1ff6aef158557c1ccd02d72e02f3
Henen <henen.dev@gmail.com>
2025-05-07 09:26:30 +0100

    Added a mobile only attribue on theAdBanner component

--- src/lib/components/AdBanner.svelte
+++ src/lib/components/AdBanner.svelte
@@ -6,6 +6,7 @@
     export let width: string | null = null;
     export let height: string | null = null;
     export let interval: number = 8000;
+    export let mobileOnly: boolean = false;
 
     let banners: any[] = [];
     let current = 0;
@@ -43,11 +44,25 @@
     const replaceExt = (url: string, ext: string) => url.replace(/\.\w+$/, `.${ext}`);
 </script>
 
-{#if banners.length > 0}
-    {#key current}
-        <div class="container {banners[current].mobile_only ? 'md:hidden' : ''}">
-            {#if banners[current].url}
-                <a href={banners[current].url} target="_blank">
+<div class="{mobileOnly ? 'hidden' : ''}">
+    {#if banners.length > 0}
+        {#key current}
+            <div class="container {banners[current].mobile_only ? 'md:hidden' : ''}">
+                {#if banners[current].url}
+                    <a href={banners[current].url} target="_blank">
+                        <picture>
+                            {#each banners[current].formats ?? [getExtension(banners[current].image)] as format}
+                                <source srcset="{replaceExt(banners[current].image, format)}?v={banners[current]._v}" type="image/{format}" />
+                            {/each}
+                            <img
+                                src="{replaceExt(banners[current].image, (banners[current].formats ?? [getExtension(banners[current].image)]).at(-1))}?v={banners[current]._v}"
+                                alt={banners[current].label ?? 'Advertisement'}
+                                style="width: {width ?? banners[current].width ?? '800px'}; height: {height ?? banners[current].height ?? '99px'};"
+                                class="mx-auto max-w-full"
+                            />
+                        </picture>
+                    </a>
+                {:else}
                     <picture>
                         {#each banners[current].formats ?? [getExtension(banners[current].image)] as format}
                             <source srcset="{replaceExt(banners[current].image, format)}?v={banners[current]._v}" type="image/{format}" />
@@ -59,24 +74,12 @@
                             class="mx-auto max-w-full"
                         />
                     </picture>
-                </a>
-            {:else}
-                <picture>
-                    {#each banners[current].formats ?? [getExtension(banners[current].image)] as format}
-                        <source srcset="{replaceExt(banners[current].image, format)}?v={banners[current]._v}" type="image/{format}" />
-                    {/each}
-                    <img
-                        src="{replaceExt(banners[current].image, (banners[current].formats ?? [getExtension(banners[current].image)]).at(-1))}?v={banners[current]._v}"
-                        alt={banners[current].label ?? 'Advertisement'}
-                        style="width: {width ?? banners[current].width ?? '800px'}; height: {height ?? banners[current].height ?? '99px'};"
-                        class="mx-auto max-w-full"
-                    />
-                </picture>
-            {/if}
-        </div>
-    {/key}
-{:else if notFound}
-    <div class="text-center text-red-600 text-sm italic">Advert “{name}” not found.</div>
-{:else}
-    <div class="text-center text-yellow-500 text-sm italic">No valid adverts for “{name}”.</div>
-{/if}
+                {/if}
+            </div>
+        {/key}
+    {:else if notFound}
+        <div class="text-center text-red-600 text-sm italic">Advert “{name}” not found.</div>
+    {:else}
+        <div class="text-center text-yellow-500 text-sm italic">No valid adverts for “{name}”.</div>
+    {/if}
+</div>
