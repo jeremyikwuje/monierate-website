a9e84a5a1fd5e8ad334cd2766a636f53eb7d64e5
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2025-11-03 08:48:06 +0100

    feat(banners): replace oneremit banner with with Spenda banner

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import AdBanner from '$lib/components/AdBanner.svelte';
+    export let data;
 </script>
 
 <div class="bg-white dark:bg-gray-800">
     <div class="container">
-       <AdBanner name="blog_home" />
+       <AdBanner name="blog_home" isMobile={data.isMobile}/>
     </div>
 </div>
 
